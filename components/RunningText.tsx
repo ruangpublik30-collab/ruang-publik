25d316a1f98ac6479de3386b55de36e99f207ff2
Afkar Mohamad <akmalnurdiansyahmohamad@gmail.com>
2026-02-27 23:04:08 +0800

    modifed running text

--- components/RunningText.tsx
+++ components/RunningText.tsx
@@ -2,17 +2,16 @@
 
 export default function RunningText() {
     return (
-        <div className="w-full bg-black border-b border-gray-800 overflow-hidden">
-            <div className="whitespace-nowrap animate-marquee py-2 text-sm sm:text-base font-normal tracking-wide text-white">
+        <div className="w-full bg-neutral-950 border-b border-neutral-800 overflow-hidden">
+            <div className="whitespace-nowrap animate-marquee py-2 text-sm sm:text-base italic font-medium tracking-wide text-yellow-400">
 
-                <span className="mx-8 text-gray-500">●</span>
+                <span className="mx-8 text-yellow-600">●</span>
                 Ruang Publik — Portal Artikel Opini, Analisis Sosial, Ekonomi, dan Refleksi Spiritual yang Membuka Wawasan dan Memperluas Perspektif Publik Indonesia.
 
-                <span className="mx-8 text-gray-500">●</span>
+                <span className="mx-8 text-yellow-600">●</span>
                 Ruang Publik — Portal Artikel Opini, Analisis Sosial, Ekonomi, dan Refleksi Spiritual yang Membuka Wawasan dan Memperluas Perspektif Publik Indonesia.
 
             </div>
         </div>
     )
 }
-    
